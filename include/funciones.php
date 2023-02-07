c5c47f7db38feeb1b91483cc4d4a750ee5c3d411
Marco Hernandez <marco7245@hotmail.com>
2023-02-06 18:56:56 -0600

    Cambios Css

--- include/funciones.php
+++ include/funciones.php
@@ -5,28 +5,25 @@ function mostrarCandidatos()
     require './include/Conexion.php';
     $result = mysqli_query($db, "SELECT * FROM partido");
 
-    echo "<table border='1'>";
-    echo "<tr>
-        <td>ID</td>
-        <td>Partido</td>
-        <td>Nombre</td>
-        <td>Candidato</td>
-        <td>Imagen</td>
-      </tr>";
+    echo "<section class='Plantilla'>"; 
+    echo "<h2>Plantilla de Candidatos</h2>";
+    echo "<div class='candidatos'>";
 
     while ($row = mysqli_fetch_array($result)) {
-        $id = $row['id'];
         $siglas = $row['inicial'];
         $nombre_partido = $row['nombre'];
         $candidato = $row['candidato'];
         $imagen = $row['imagen'];
-        echo "<tr>";
-        echo "<td>$id</td>
-          <td>$siglas</td>
-          <td>$nombre_partido</td>
-          <td>$candidato</td>
-          <td><img width='70' heigth='70' src='misImagenes/$imagen'></td>";
-        echo "</tr>";
+        echo "<div>";
+
+        echo "<img src='./img/$imagen'>";
+        echo "<p>$nombre_partido ($siglas) </p>";
+        echo "<p>$candidato</p>";
+        echo "";
+
+        echo "</div>";
     }
-    echo "</table>";
+
+    echo "</div>";
+    echo "</section>";
 }
--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="preload" href="./css/normalize.css" as="style">
     <link rel="stylesheet" href="./css/normalize.css">
+    
+    <link rel="preload" href="./css/styles.css" as="style">
     <link rel="stylesheet" href="./css/styles.css">
 
     <title>Elecciones 2023</title>
@@ -24,6 +27,7 @@
     require './include/funciones.php';
 
     mostrarCandidatos();
+    
     ?>
 
 </body>
